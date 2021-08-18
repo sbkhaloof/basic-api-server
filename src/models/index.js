59b90289a0e0e23a9ee074eaf68e88144fe9077a
siham khaloof <sehammalkawi92@gmail.com>
2021-08-18 14:22:29 +0300

    edit

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,4 @@
-const POSTGRES_URI = process.env.POSTGRES_URI || "postgres://pysexuvgrdykrz:85d94f3c3af560eb1d5a277a2fea5d2c0d81cac2d4888630917f9ca8d1322538@ec2-107-21-10-179.compute-1.amazonaws.com:5432/d762pqmb621fmn";
+const POSTGRES_URI = process.env.DATABASE_URL || "postgres://siham@localhost:5432/food";
 const { Sequelize, DataTypes } = require("sequelize");
 const Food = require('./food');
 const Clothes=require('./clothes')
@@ -10,6 +10,3 @@ module.exports = {
     food: Food(sequelize, DataTypes),
     clothes:Clothes(sequelize, DataTypes)
 }
-
-
-// postgres://siham@localhost:5432/food
