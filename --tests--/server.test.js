aea9d7f011919fac4233b1ceb41ff3a83ead4d00
siham khaloof <sehammalkawi92@gmail.com>
2021-08-21 13:33:14 +0300

    editing

--- --tests--/server.test.js
+++ --tests--/server.test.js
@@ -1,99 +1,91 @@
-'use strict'
+'use strict';
+
 const server = require('../src/server');
 const supertest = require('supertest');
+
+
 const request = supertest(server.server);
 
+describe('middleware',()=>{
+    describe('api server',()=>{
 
-describe('express serve', () => {
-
-    describe('API food', () => {
-
-        it("food get test", async () => {
-            const response = await request.get(`/food`);
-            expect(response.status).toEqual(200);
-        });
-
-        it('check post food ', async () => {
-            const reqBody = {
-                "foodName": "euwwwuue",
-                "foodType": "oooooooo",
-            }
-            const response = await request.post('/food').send(reqBody);
-            expect(response.status).toEqual(201);
-        });
-
-        it("check update food", async () => {
-
-            const response = await request.put('/food/3');
-            expect(response.status).toEqual(200);
-
-        });
-        it("food delete test", async () => {
-            const response = await request.delete(`/food/2`);
-            expect(response.status).toEqual(204);
-        });
-
-    })
-    describe('api clothes', () => {
-        it('clothes get test', async () => {
-            const response = await request.get(`/clothes`);
-            expect(response.status).toEqual(200);
-        });
-        it('check post clothes ', async () => {
-            const reqBody = {
-                "name": "euwwwuue",
-                "type": "oooooooo",
-            }
-            const response = await request.post('/clothes').send(reqBody);
-            expect(response.status).toEqual(201);
-        });
-        it("check update clothes", async () => {
-
-            const response = await request.put('/clothes/3');
-            expect(response.status).toEqual(200);
-
-        });
-        it("clothes delete test", async () => {
-            const response = await request.delete(`/clothes/2`);
-            expect(response.status).toEqual(204);
-        });
-
-    })
-    describe('Api server', () => {
-
-        it('should check the Every Thing Is Working Good', async () => {
+        it('should check eveery thing is work good',async()=>{
             //arrange
-            let param = '/';
-            let status = 200;
-            let text = 'Every Thing Is Working Good';
+            let status=200;
+            let param='/';
+            let text='Every Thing Is Working Good';
             //act
-            const response = await request.get(param);
+            const response=await request.get(param);
             //assert
             expect(response.status).toBe(status);
             expect(response.text).toBe(text);
-
-        });
-        it("handles not found request", async () => {
-            const response = await request.get("/notttfouuundreqquuest");
-            expect(response.status).toEqual(404);
-        });
-
-        // bad method
-        it("handles my internal server errors", async () => {
-            const response = await request.post("/bad");
-            expect(response.status).toEqual(404);
-        });
-         // bad method
-    it("handles my internal server errors", async () => {
-        const response = await request.get("/badmethod");
-        expect(response.status).toEqual(500);
-      });
+        })
+        it('should check error 404 not found',async()=>{
+            let status=404;
+            let param='/notfound';
+            const response=await request.get(param);
+            expect(response.status).toBe(status);
+            
+        })
+        it('should check internal server error 500',async()=>{
+            let status=500;
+            let param='/bad';
+            const response=await request.get(param);
+            expect(response.status).toBe(status)
+        })
     })
-})
+    describe('food server',()=>{
+        let obj={
+            "foodName": "euwwwuue",
+            "foodType": "oooooooo",
+        }
+       it('should test get food',async()=>{
+           const response=await request.get('/food');
+           expect(response.status).toEqual(200);
+       })
+       it('should test post food',async()=>{
+        const reqBody = {
+            "foodName": "euwwwuue",
+            "foodType": "oooooooo",
+        }
+        const response = await request.post('/food').send(reqBody);
+        expect(response.status).toEqual(201);
+       })
+       it('should test put food',async()=>{
+        const response = await request.put('/food/3').send(obj);
+        expect(response.status).toEqual(200);
+       })
+       it("should test food delete ", async () => {
+        const response = await request.delete(`/food/2`);
+        expect(response.status).toEqual(204);
+    });
+        });
+        describe('clothes server',()=>{
+            const cloObj={
+                "name": "euwwwuue",
+                "type": "oooooooo",
+            }
+            it('should test get clothes',async()=>{
+                const response=await request.get('/clothes');
+                expect(response.status).toEqual(200);
+            })
+            it('should test post clothes',async()=>{
+                const reqBody = {
+                    "name": "euwwwuue",
+                    "type": "oooooooo",
+                }
+                const response = await request.post('/clothes').send(reqBody);
+                expect(response.status).toEqual(201);
+            })
+            it('should check put clothes',async()=>{
+                const response = await request.put('/clothes/3').send(cloObj);
+            expect(response.status).toEqual(200);
 
-
-
-
-
-
-
+            })
+            it("clothes delete test", async () => {
+                const response = await request.delete(`/clothes/2`);
+                expect(response.status).toEqual(204);
+            });
+        })
+    })
+    
--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -1,11 +1,20 @@
 'use strict'
 
-module.exports=(req,res,next)=>{
-    req.name=req.query.name
-    let name=req.name
-    if(typeof name === 'string' && name.length !==0){
-        next();
-    } else{
-        next('its not a name :(')
-    }
-}
+// module.exports=(req,res,next)=>{
+//     req.name=req.query.name
+//     let name=req.name
+//     if(typeof name === 'string' && name.length !==0){
+//         next();
+//     } else{
+//         next('its not a name :(')
+//     }
+// }
+
+
+const loggerMiddleware = (req, res, next) => {
+  console.log("Request Info", req.method, req.path);
+
+  next();
+};
+
+module.exports = loggerMiddleware;
